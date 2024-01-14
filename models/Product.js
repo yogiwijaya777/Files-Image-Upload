e85456c6fa87985621f9bd83ec2e7149629d7660
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-01-14 11:12:22 +0700

    Procut schema

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
